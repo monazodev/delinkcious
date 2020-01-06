19f063fd702d36053dfbf038d7ab7a0af5a380bd
monazodev <monazoit@gmail.com>
2020-01-06 23:54:05 +0100

    Push images

--- build.sh
+++ build.sh
@@ -22,7 +22,7 @@ for svc in *; do
     echo "stable image: ${STABLE_IMAGE}"
     docker build -t "$IMAGE" .
     docker tag "${IMAGE}" "${STABLE_IMAGE}"
-    # docker push "${IMAGE}"
-    # docker push "${STABLE_IMAGE}"
+    docker push "${IMAGE}"
+    docker push "${STABLE_IMAGE}"
 done
 cd $ROOT_DIR
